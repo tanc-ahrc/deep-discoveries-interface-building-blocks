e8bfab93fb74d618e1be585c39583bcee37429df
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2020-07-17 15:29:50 +0100

    Updates

---
+++ precache-manifest.643621b023d43b8302e07cd66782acbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413d1e86444d413c7411386dafc2cd31",
+    "url": "/deep-discoveries-interface-building-blocks/index.html"
+  },
+  {
+    "revision": "9e37efd4c94497efa7cb",
+    "url": "/deep-discoveries-interface-building-blocks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4fef038027574a4dd488",
+    "url": "/deep-discoveries-interface-building-blocks/static/js/2.926e0527.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deep-discoveries-interface-building-blocks/static/js/2.926e0527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e37efd4c94497efa7cb",
+    "url": "/deep-discoveries-interface-building-blocks/static/js/main.02b5e48e.chunk.js"
+  },
+  {
+    "revision": "a9a0bc4a6fe1d8c85143",
+    "url": "/deep-discoveries-interface-building-blocks/static/js/runtime-main.a7fd9788.js"
+  }
+]);
